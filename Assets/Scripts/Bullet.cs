1a8599ac7b86c4301ea72120ad7c8214424433b1
StainBlack <sebahcavalcante073@gmail.com>
2025-10-10 14:19:30 -0300

    Update Bullet Script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,8 +4,14 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed;
 
+    public float BulletSpeed
+    {
+        get { return speed; }
+        set {speed = value; }
+    }
+    
     void Update()
     {
-        transform.Translate(Vector3.left * (speed * Time.deltaTime));
+        transform.Translate(Vector3.down * (speed * Time.deltaTime));
     }
 }
